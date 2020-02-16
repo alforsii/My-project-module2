4bcdf04a7c6f8e9ea7c4673163859c3611c6fb42
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-16 02:57:58 -0500

    replace files with actual project files

--- app.js
+++ app.js
@@ -1,58 +1,56 @@
 require('dotenv').config();
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
 const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-mongoose
-  .connect('mongodb://localhost/project', { useNewUrlParser: true })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+const flash = require('connect-flash');
+const hbs = require('hbs');
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
-);
+const userLocals = require('./configs/user-locals');
 
 const app = express();
 
-// Middleware Setup
+require('./configs/db.config');
+require('./configs/passport.config')(app);
+app.use(userLocals);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(flash());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Express View engine setup
-
-app.use(
-  require('node-sass-middleware')({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    sourceMap: true,
-  })
-);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-const index = require('./routes/index');
+const index = require('./routes/index.routes');
+const authRoutes = require('./routes/auth.routes');
 app.use('/', index);
-app.use('/', require('./routes/users/users-routes'));
+app.use('/', authRoutes);
+app.use('/', require('./routes/user-profile-routes'));
+app.use('/', require('./routes/user-post-routes'));
+app.use('/', require('./routes/user-comments-routes'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
