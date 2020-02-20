a31226e71e941fb6f0304cdc758482252c5b63d8
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-19 23:02:38 -0500

    add search list function - done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,28 @@ document.addEventListener(
   'DOMContentLoaded',
   () => {
     // console.log('IronGenerator JS imported successfully!');
+    //=-=-=-=-=-=--=-=-=-=-=Search user list---==-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-
+    document
+      .getElementById('search-user')
+      .addEventListener('keyup', filterList);
+
+    function filterList() {
+      const users = document.querySelectorAll('.user');
+      const input = document.getElementById('search-user').value.toUpperCase();
+
+      users.forEach((user, i) => {
+        const userName = user
+          .querySelector('.username')
+          .innerHTML.toUpperCase();
+        // console.log('userName: ', userName);
+        // console.log('Output for: user', user);
+
+        !userName.includes(input)
+          ? (user.parentElement.style.display = 'none')
+          : (user.parentElement.style.display = '');
+      });
+    }
+    //=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=
   },
   false
 );
