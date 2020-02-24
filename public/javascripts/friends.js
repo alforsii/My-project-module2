f62a5ec440c16ce7ef5f2e9e48735cebc51d0119
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-02-23 21:04:03 -0500

    add friends list and update

---
+++ public/javascripts/friends.js
@@ -0,0 +1,115 @@
+(function() {
+  if (document.getElementById('messageBoard')) {
+    // Here is the current user from back end I passed to layout inside html tag as attribute to use in front end
+    let userInSessionID = document
+      .getElementsByTagName('html')[0]
+      .getAttribute('userInSession');
+
+    // Gets elem by id with helper function
+    let element = id => document.getElementById(id);
+
+    // Get message board elements that we need
+    let friendsList = element('friends-list');
+
+    //Connect to socket.io
+    //=-=-=-=-===-=-=-=-=-=-= Socket event listener -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // 1.
+    let socket = io();
+    window.onunload = () => socket.close();
+    // 2.Another way of connecting
+    // let socket = io.connect(`http://127.0.0.1:${parseInt(port)}`);
+
+    // console.log('Output for: port', parseInt(port));
+    const users = document.querySelectorAll('.user');
+    const addBtns = document.querySelectorAll('.add-friend');
+
+    //=-=-=-=-===-=-=-=-=-=-= Loop through users list -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // If any user selected by click then display message board
+
+    addBtns.forEach(btn => {
+      btn.addEventListener('click', event => {
+        event.preventDefault();
+        // console.log(btn.getAttribute('user_id'));
+        const user_id = btn.getAttribute('user_id');
+        socket.emit('display-user', [userInSessionID, user_id]);
+      });
+    });
+
+    //Set default status
+    let statusDefault = status.textContent;
+    //=-=-=-=-===-=-=-=-=-=-= Send Status to the current User -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    //Reusable Send status function
+    let setStatus = s => {
+      status.textContent = s;
+      if (s !== statusDefault) {
+        let delay = setTimeout(() => {
+          setStatus(statusDefault);
+        }, 3000);
+      }
+    };
+
+    //------- Get status from server ---------
+    //'status' that we are calling is a function that we assigned in the socket property back end)
+    socket.on('status', data => {
+      // data ={ this data will be sent from server when msg is sent
+      //   message: 'Message sent',
+      //   sent: true,
+      // };
+      //get message status
+      // - here we're passing the status data from back end to the setStatus function that we created earlier
+      //if data object then get message property else just send data itself(whatever is)
+      setStatus(typeof data === 'object' ? data.message : data);
+      //so if message was sent then clear the input field
+      if (data.sent) textarea.value = '';
+    });
+
+    //Send current user id to get current user friends list from DB
+    window.onload = () => {
+      socket.emit('redisplay-friends-list', {
+        userId: userInSessionID,
+      });
+    };
+    //Receive back current users friends list and display
+    socket.on('output-friends', friendsFromDB => {
+      for (let i = 0; i < friendsFromDB.length; i++) {
+        createFriend(friendsFromDB[i]);
+      }
+    });
+
+    //check connection if it's not undefined to avoid getting an error
+    //=-=-=-=-===-=-=-=-=-=-= Receive back newlyCreatedFriend -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    if (socket !== undefined) {
+      console.log('Connected to socket to friend.js..');
+      socket.on('display-added-friend', newlyCreatedFriend => {
+        createFriend(newlyCreatedFriend);
+      });
+    }
+
+    //Create newFriend helper function
+    function createFriend(newlyCreatedFriend) {
+      const {
+        _id,
+        username,
+        firstName,
+        lastName,
+        email,
+        path,
+        imageName,
+        friends,
+      } = newlyCreatedFriend;
+      const newFriend = document.createElement('div');
+      newFriend.setAttribute('class', 'each-friend');
+      newFriend.innerHTML = `
+          <a href="">
+             <img class="chat-users-small" src="${path}" alt="${username}">
+          </a>
+          <a class="username" href="" _id="${_id}" _username="${username}">
+            ${firstName} ${lastName}
+          </a>
+        `;
+      friendsList.appendChild(newFriend);
+    }
+
+    // <-- end of function -->
+  }
+})();
---
+++ routes/socket/friends.io.js
@@ -0,0 +1,142 @@
+module.exports = client => {
+  const Chat = require('../../models/Chat.model');
+  const Message = require('../../models/Message.model');
+  const User = require('../../models/User.model');
+  const Friend = require('../../models/Friend.model');
+
+  //if user signed in (it's set to signed in - where message board)
+  client.on('connection', socketIO => {
+    // console.log('Output for: socketIO', socketIO);
+    console.log('A friends.io.js connected');
+    // console.log('new connection: ' + socketIO.id);
+
+    socketIO.on('redisplay-friends-list', data => {
+      // console.log('data', data);
+      //Get current user in session id and check in User.friends if he has any friends
+      //if current user has friend or friends then send them to the client(front end to display)
+      User.findById(data.userId)
+        .populate('friends')
+        .then(userFromDB => {
+          const { friends } = userFromDB;
+          // console.log('userFromDB: ', friends);
+          if (friends.length !== 0) {
+            socketIO.emit('output-friends', friends); //
+          }
+        })
+        .catch(err =>
+          console.log(`Error while getting friends list from DB: ${err}`)
+        );
+    });
+
+    //Display newly added friend
+    socketIO.on('display-user', usersData => {
+      //1.Current user in session
+      User.findById(usersData[0])
+        .then(userInSessionFromDB => {
+          const {
+            _id,
+            username,
+            firstName,
+            lastName,
+            email,
+            path,
+            imageName,
+            friends,
+          } = userInSessionFromDB;
+
+          //Create current user(ourself) as a new friend to other User friends list
+          Friend.create({
+            username,
+            firstName,
+            lastName,
+            email,
+            path,
+            imageName,
+            friends,
+          })
+            .then(newlyCreatedFriend => {
+              console.log('Output for: newlyCreatedFriend');
+              //call addToFriendsList and pass  newlyCreatedFriend for current user
+              // and pass the other user id to add (newlyCreatedFriend) current user as a friend to other users friends list
+              addToFriendsList(newlyCreatedFriend, usersData[1]); //
+            })
+            .catch(err =>
+              console.log(`Error while creating a new friend ${err}`)
+            );
+          //end Friend.creat()
+        })
+        .catch(err =>
+          console.log(
+            `Error while getting specific user from friends.io.js ${err}`
+          )
+        );
+      //end of current user
+
+      //2.Other user
+      User.findById(usersData[1])
+        .then(otherUserFromDB => {
+          const {
+            _id,
+            username,
+            firstName,
+            lastName,
+            email,
+            path,
+            imageName,
+            friends,
+          } = otherUserFromDB;
+
+          //Create a new Friend
+          Friend.create({
+            username,
+            firstName,
+            lastName,
+            email,
+            path,
+            imageName,
+            friends,
+          })
+            .then(newlyCreatedFriend => {
+              console.log('Output for: newlyCreatedFriend');
+              //call addToFriendsList and pass  newlyCreatedFriend for current user
+              // and pass the other user id to add (newlyCreatedFriend) current user as a friend to other users friends list
+              addToFriendsList(newlyCreatedFriend, usersData[0]); //
+              //and here also we're sending other user as a newlyCreatedFriend to display in our friends list
+              socketIO.emit('display-added-friend', newlyCreatedFriend);
+            })
+            .catch(err =>
+              console.log(`Error while creating a new friend ${err}`)
+            );
+          //end Friend.creat()
+        })
+        .catch(err =>
+          console.log(
+            `Error while getting specific user from friends.io.js ${err}`
+          )
+        );
+    });
+
+    //Add to friends list function
+    function addToFriendsList(newlyCreatedFriend, userId) {
+      User.findByIdAndUpdate(
+        userId,
+        {
+          $push: { friends: newlyCreatedFriend._id },
+        },
+        { new: true }
+      )
+        .then(updatedUser => {
+          console.log('updatedUser: ');
+        })
+        .catch(err =>
+          console.log(`Error while trying to update friends list ${err}`)
+        );
+      //end of User.findByIdAndUpdate(userId)
+    }
+
+    //------- Disconnected ---------------------------
+    socketIO.on('disconnect', function() {
+      console.log('disconnect: ' + socketIO.id);
+    });
+  }); //end socketIO connection
+};
